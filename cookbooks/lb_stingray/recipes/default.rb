ca312ebf89ae93c5e1b72e494d42f1d0cb142928
Matt Geldert <mgeldert@riverbed.com>
2013-03-14 16:03:33 +0000

    Trigger install from our recipe instead of lb::install_server recipe

--- cookbooks/lb_stingray/recipes/default.rb
+++ cookbooks/lb_stingray/recipes/default.rb
@@ -16,20 +16,6 @@ end
 log "Override load balancer to use Stingray."
 node[:lb][:service][:provider] = "lb_stingray"
 
-# 2D array of pools
-# Example: [["_serverid", "/serverid"], ["_appsever", "/appsever"], ["default", "default"]]
-pool_list = node[:lb][:pools].gsub(/\s+/, "").split(",").uniq.map { |pool| [ pool.gsub(/[\/]/, '_'), pool ] }
-
-pool_list.each do |pool_name_short, pool_name_full|
-  log "  Setup default load balancer resource for pool '#{pool_name_short}'."
-  log "  load balancer pool full name is '#{pool_name_full}'."
-
-  lb pool_name_short do
-    provider "lb_stingray"
-    pool_name_full pool_name_full
-    persist true # Store this resource in node between converges.
-    action :nothing
-  end
-end
+action :install
 
 rightscale_marker :end
