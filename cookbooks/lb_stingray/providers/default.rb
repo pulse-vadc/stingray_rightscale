656dd966625778ef8a0b4c4be70eab0aca16b130
Matt Geldert <mgeldert@riverbed.com>
2013-03-15 11:47:13 +0000

    Fixed untar'd directory name (it's not the same as packagename is the Gold package is used)

--- cookbooks/lb_stingray/providers/default.rb
+++ cookbooks/lb_stingray/providers/default.rb
@@ -83,10 +83,10 @@ action :install do
         #log "Performing initial configuration..."
         command "\
         tar xzvf #{packagename}.tgz &&
-        #{packagename}/zinstall --replay-from=/tmp/install_replay"
+        ZeusTM_#{version}_Linux-#{arch}/zinstall --replay-from=/tmp/install_replay"
         notifies :delete, resources(
             :file => "/tmp/#{packagename}.tgz",
-            :directory => "/tmp/#{packagename}",
+            :directory => "/tmp/ZeusTM_#{version}_Linux-#{arch}",
             :template => "/tmp/install_replay"
         ), :delayed
         #log "Initial configuration completed!"
