90258e2336790f6c0e107d1d3dfcdf57aa6010a4
Matt Geldert <mgeldert@riverbed.com>
2013-03-18 13:04:43 +0000

    Add a default 'lb/pool_name' value as boot fails without it

--- cookbooks/lb_stingray/recipes/default.rb
+++ cookbooks/lb_stingray/recipes/default.rb
@@ -13,23 +13,27 @@ class Chef::Recipe
    include RightScale::App::Helper
 end
 
-log "Override load balancer to use Stingray."
+log "Override load balancer to use Riverbed Stingray."
 node[:lb][:service][:provider] = "lb_stingray"
 
 if node[:lb][:pools] != nil
     pool_list = node[:lb][:pools].gsub(/\s+/, "").split(",").uniq.map { |pool| [ pool.gsub(/[\/]/, '_'), pool ] }
-
-    pool_list.each do |pool_name_short, pool_name_full|
-        log "  Setup default load balancer resource for pool '#{pool_name_short}'."
-        log "  load balancer pool full name is '#{pool_name_full}'."
-
-        lb pool_name_short do
-            provider "lb_stingray"
-            pool_name_full pool_name_full
-            persist true # Store this resource in node between converges.
-            action :nothing
-         end
-    end
+else
+    log "Creating a 'default' virtual server because none was specified in the 'lb/pool_name' input and boot will fail without one" 
+    pool_list = ["default"]
 end
+
+pool_list.each do |pool_name_short, pool_name_full|
+    log "  Setup default load balancer resource for pool '#{pool_name_short}'."
+    log "  load balancer pool full name is '#{pool_name_full}'."
+
+    lb pool_name_short do
+        provider "lb_stingray"
+        pool_name_full pool_name_full
+        persist true # Store this resource in node between converges.
+        action :nothing
+     end
+end
+
 
 rightscale_marker :end
